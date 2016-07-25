284b90c4ee812c7808388e8958ca86c1c131ea99
a93794 <a93794@02LAB108PC09.rn.senac.br>
2016-07-25 19:16:58 -0300

    TarefaCasa

---
+++ src/controller/UsuarioController.java
@@ -0,0 +1,43 @@
+
+package controller;
+
+import dao.UsuarioDao;
+import model.Usuario;
+
+
+public class UsuarioController {
+    private final UsuarioDao dao;
+    
+    public UsuarioController() {
+        dao = new UsuarioDao();
+    }
+    
+    public boolean cadastrar(Usuario usuario) {
+        if (dao.findByLogin(usuario.getLogin()) == null) {
+            if (dao.create(usuario)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean login(String login, String senha) {
+        Usuario usuario = dao.findByLogin(login);
+        if (usuario != null && usuario.getLogin().equals(login) && usuario.getSenha().equals(senha)) {
+            return true;
+        }
+        return false;
+    }
+    
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
---
+++ src/dao/PersistDB.java
@@ -0,0 +1,11 @@
+
+package dao;
+
+
+public interface PersistDB {
+    
+    public int getId();
+    public void setId(int id);
+    
+    
+}
